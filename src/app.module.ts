95297e1853f3398ac54511a1bc6043ba65556531
Mohammed <mohammed.ammourie@gmail.com>
2024-11-06 13:42:52 +0300

    sqllite

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,22 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-
+import { BlogsModule } from './blogs/blogs.module';
+import { UsersModule } from './users/users.module';
+import { Blog } from './blogs/entities/blog.entity';
+import { User } from './users/entities/user.entity';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [Blog, User],
+      synchronize: true,
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    BlogsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/blogs/entities/blog.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Blog {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  author: string;
+
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+}
