9a62426d796a484afa6b60710f189e19979fea00
Mohammed <mohammed.ammourie@gmail.com>
2024-11-06 11:24:45 +0300

    fix swagger

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,18 @@ async function bootstrap() {
     .build();
   app.useGlobalPipes(new ValidationPipe());
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-  await app.listen(process.env.APP_PORT ?? 3000);
+  SwaggerModule.setup("docs", app, documentFactory, {
+    customSiteTitle: "Api Docs",
+    customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
+    customCssUrl: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    ],
+  }); await app.listen(process.env.APP_PORT ?? 3000);
 }
 bootstrap();
