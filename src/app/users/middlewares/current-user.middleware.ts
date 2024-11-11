ed5061e211984fccc8e114b62edfa2a374a9e785
Mohammed <mohammed.ammourie@gmail.com>
2024-11-11 13:43:21 +0300

    fixes

--- src/app/users/middlewares/current-user.middleware.ts
+++ src/app/users/middlewares/current-user.middleware.ts
@@ -2,17 +2,26 @@ import { NestMiddleware } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../users.service';
-
+import { User } from '../entities/user.entity';
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser: User;
+      session: Session;
+    }
+    interface Session {
+      user: User;
+    }
+  }
+}
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private readonly userService: UsersService) {}
   async use(req: Request, res: Response, next: NextFunction) {
-    //@ts-ignore
     const { id } = req.session.user || {};
 
     if (id) {
       const user = await this.userService.findOne(id);
-      //@ts-ignore
       req.currentUser = user;
     }
 
